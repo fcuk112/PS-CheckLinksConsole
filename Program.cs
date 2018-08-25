bb7f8a0d7688391946fd415cf98cb9b2d40faaa7
Frank Cheung <fcuk112@gmail.com>
2018-08-25 15:39:14 +0100

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace CheckLinksConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var site = "https://www.google.com";
+            var client = new HttpClient();
+            var body = client.GetStringAsync(site);
+            Console.WriteLine(body.Result);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Links");
+            var links = LinkChecker.GetLinks(body.Result);
+            links.ToList().ForEach(Console.WriteLine);
+
+        }
+    }
+}
